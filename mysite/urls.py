78f41dea27ce3e07472c139ccca3dee9d5fe1d24
Andreu Vallbona Plazas <avallbona@gmail.com>
2018-10-05 13:55:00 +0200

    more stuff

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,11 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
 
 urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    # import debug_toolbar
+    from django.views import static
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', static.serve, {'document_root': settings.MEDIA_ROOT}),
+        url(r'^static/(?P<path>.*)$', static.serve, {'document_root': settings.STATIC_ROOT}),
+    ]
+
+    # urlpatterns = [url(r'^__debug__/', include(debug_toolbar.urls))] + urlpatterns
